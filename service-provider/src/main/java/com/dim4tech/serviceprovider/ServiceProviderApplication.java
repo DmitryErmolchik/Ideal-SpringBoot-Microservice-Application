72307e1b527e9dd3c57613baf797ef1ab8cdcef6
Dmitry Ermolchik <dimer@dim4tech.com>
2020-05-13 18:00:02 -0400

    Initial commit

---
+++ service-provider/src/main/java/com/dim4tech/serviceprovider/ServiceProviderApplication.java
@@ -0,0 +1,16 @@
+package com.dim4tech.serviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderApplication {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < "".toCharArray().length; i++) {
+			
+		}
+		SpringApplication.run(ServiceProviderApplication.class, args);
+	}
+
+}
